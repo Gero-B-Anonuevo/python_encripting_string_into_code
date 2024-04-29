308be9da38982b8cda5c2106b357945c83fb5271
Anonuevo <geroanonuevo3@gmail.com>
2024-04-29 11:00:13 +0800

    finish

---
+++ prog.py
@@ -0,0 +1,7 @@
+word = input("Please insert a sentence: ")
+change_a = word.replace('a', '*')
+change_e = change_a.replace('e', '&')
+change_i = change_e.replace('i', '#')
+change_o = change_i.replace('o', '+')
+change_u = change_o.replace('u', '!')
+print(f"Your new word: {change_u}")
